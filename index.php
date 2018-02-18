c8a1a8659ade0d8312eeb7dcb807400c0be9b424
rcampusano <rcampusano@rita>
2018-02-18 11:52:32 -0300

    fue la jhanndy

--- index.php
+++ index.php
@@ -10,6 +10,7 @@ and open the template in the editor.
         <title></title>
     </head>
     <body>
+        <h1> hola jhanndy </h1>
         <?php
         // put your code here
         $rut = 17019107;
